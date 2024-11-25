984fd500f7da8a9c2063054c60c91f367a4d4548
Quinn Bowers <quinn@robotics88.com>
2024-11-25 15:29:00 -0500

    remove unnecessary prints

--- utils/geotiff_utils.py
+++ utils/geotiff_utils.py
@@ -4,14 +4,7 @@ import rasterio ,rasterio.warp
 
 def get_lat_long_bounds(geotiff_path):
     with rasterio.open( geotiff_path ) as geotiff_file:
-        # transform to EPSG:4328 (Geocentric lat long)
-
-        print(f'Original bounds in {geotiff_file.crs}')
-        print(geotiff_file.bounds)
-        print(f'Converted bounds in {rasterio.crs.CRS.from_epsg(4326)}')
-        print(rasterio.warp.transform_bounds(geotiff_file.crs, rasterio.crs.CRS.from_epsg(4326), *geotiff_file.bounds))
-    
-
+        # transform to EPSG:4326 (Geocentric lat long)
         return rasterio.warp.transform_bounds(geotiff_file.crs, rasterio.crs.CRS.from_epsg(4326), *geotiff_file.bounds)
     
 
