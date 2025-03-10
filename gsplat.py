3d85eb9a550d82d98edcc4766d1c0a55751358b1
Quinn <quinn@robotics88.com>
2025-03-10 15:07:46 +0000

    add logging

--- gsplat.py
+++ gsplat.py
@@ -6,6 +6,7 @@ import docker.types
 
 from pathlib import Path
 
+import time
 
 def generate_images(video_path, images_path, sample_rate, image_name_pattern):
     images_path.mkdir(parents=True, exist_ok=True)
@@ -29,24 +30,46 @@ def generate_sparse(images_path, database_path, sparse_path):
     mapper_command = 'colmap mapper --database_path /data/database.db --image_path /data/images --output_path /data/sparse'
 
     # TODO: pull first, make logs visible
-    # gpu_device = docker.types.DeviceRequest(count=-1, capabilities=[['gpu']])
-    # device_requests=[gpu_device]
+    gpu_device = docker.types.DeviceRequest(count=-1, capabilities=[['gpu']])
+    device_requests=[gpu_device]
 
     # TODO: consider capping nvidia clock speeds, lest the computer crash
     # sudo nvidia-smi -lgc 300,1500
 
-    colmap_container = client.containers.run('colmap/colmap:latest', 'sleep infinity', auto_remove=False, detach=True, runtime='nvidia', mounts=[images_mount, sparse_mount, db_mount])
+    # colmap_container = client.containers.run('colmap/colmap:latest', 'sleep infinity', auto_remove=False, detach=True, mounts=[images_mount, sparse_mount, db_mount])
+    print('running feature extraction...')
+    colmap_container = client.containers.run('colmap/colmap:latest', feature_extraction_command, auto_remove=True, runtime='nvidia', detach=True, device_requests=[gpu_device], mounts=[images_mount, sparse_mount, db_mount])
+
+    log_stream = colmap_container.attach(stream=True, logs=True)
+    colmap_container.start()
+    for log_line in log_stream:
+        print(str(log_line))
+
+    print('running matcher...')
+    colmap_container = client.containers.run('colmap/colmap:latest', matcher_command, auto_remove=True, runtime='nvidia', detach=True, device_requests=[gpu_device], mounts=[images_mount, sparse_mount, db_mount])
+
+    log_stream = colmap_container.attach(stream=True, logs=True)
+    colmap_container.start()
+    for log_line in log_stream:
+        print(str(log_line))
+
+    print('running mapper...')
+    colmap_container = client.containers.run('colmap/colmap:latest', mapper_command, auto_remove=True, runtime='nvidia', detach=True, device_requests=[gpu_device], mounts=[images_mount, sparse_mount, db_mount])
+
+    log_stream = colmap_container.attach(stream=True, logs=True)
+    colmap_container.start()
+    for log_line in log_stream:
+        print(str(log_line))
 
     # print('running feature extraction...')
-    print('running feature extraction...')
-    colmap_container.exec_run(feature_extraction_command)
-    print('running matcher...')
-    colmap_container.exec_run(matcher_command)
-    print('running mapper...')
-    colmap_container.exec_run(mapper_command)
+    # colmap_container.exec_run(feature_extraction_command)
+    # print('running matcher...')
+    # colmap_container.exec_run(matcher_command)
+    # print('running mapper...')
+    # colmap_container.exec_run(mapper_command)
 
-    colmap_container.kill()
-    colmap_container.remove(force=True)
+    # colmap_container.kill()
+    # colmap_container.remove(force=True)
     
 
 def generate_ply(images_path, sparse_path, ply_path, num_splats):
@@ -90,11 +113,23 @@ if __name__ == '__main__':
     database_path = Path('gsplat_data/output/gopro/database.db')
     ply_path = Path('gsplat_data/output/gopro/splat.ply')
 
+    tic = time.time()
     if not images_path.exists():
-        generate_images(video_path, images_path, 60, "out%d.png")
+        generate_images(video_path, images_path, 5, "out%d.png")
     
+    generate_images_time = time.time() - tic
+
+    tic = time.time()
     if not sparse_path.exists():
         generate_sparse(images_path, database_path, sparse_path)
+    generate_sparse_time = time.time() - tic
 
+    tic = time.time()
     if not ply_path.exists():
-        generate_ply(images_path, sparse_path, ply_path, 15000)
+        generate_ply(images_path, sparse_path, ply_path, 100_00)
+    generate_ply_time = time.time() - tic
+
+    print(f'generate_images_time: {generate_images_time:.2f}')
+    print(f'generate_sparse_time: {generate_sparse_time:.2f}')
+    print(f'generate_ply_time:    {generate_ply_time:.2f}')
+    print(f'total time elapsed:   {(generate_images_time + generate_sparse_time + generate_ply_time):.2f}')
