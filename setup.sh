f4f7b8be387cfac9ef2c43756c82f9541fed05a8
Quinn Bowers <quinn@robotics88.com>
2025-03-25 11:18:29 -0400

    add nvidia repository

--- setup.sh
+++ setup.sh
@@ -21,6 +21,12 @@ if ! command -v docker &> /dev/null; then
 fi
 
 # Install dependencies
+# Configure nvidia dep
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
 # do not use -y; give the user a chance to review dependencies before they install
 sudo apt install $packages_to_install
 
