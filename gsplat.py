6b291f2cc02b22fca1775a3a78d07d116e0e4961
Quinn <quinn@robotics88.com>
2025-03-25 08:57:54 -0400

    Add ODM support (#2)
    * add ODM support
    * restore colmap option
    * Add commandline arguments and logging

--- gsplat.py
+++ gsplat.py
@@ -1,40 +1,66 @@
 import argparse
 import subprocess
+from pathlib import Path
+import time
+
+import utils.argument_actions
 
 import docker
 import docker.types
 
-from pathlib import Path
+from fastlog import log
 
-import time
-
-
-
+log_level_options = [log.WARNING, log.INFO, log.DEBUG]
 
 
 def run_in_docker(command, image, mounts):
     # TODO: consider capping nvidia clock speeds, lest the computer crash
     # sudo nvidia-smi -lgc 300,1500
 
+    # TODO: design graceful exit - stop the container if the script is interrupted
+
     client = docker.from_env()
     gpu_device = docker.types.DeviceRequest(count=-1, capabilities=[['gpu']])
 
-    container = client.containers.run(image, command, auto_remove=True, runtime='nvidia', detach=True, device_requests=[gpu_device], mounts=mounts)
+    container = client.containers.run(image, command, auto_remove=True, runtime='nvidia', detach=True, device_requests=[gpu_device], mounts=mounts, environment={'PYTORCH_CUDA_ALLOC_CONF':'expandable_segments:True'})
     log_stream = container.attach(stream=True, logs=True)
     container.start()
 
+    # TODO: make a fastlog pipe so I don't have to do these shenanigans
+
+
+
+    # --- LOGGING ---
+    # A given element from the log stream may include any number of newlines
+    # Each newline should correspond to exactly one log statement
+    # Lastly, an element may not start or end with a newline, leaving a remnant that should be concatenated with the beginning of the next element
+    
+    remnant = ''
     for log_line in log_stream:
-        print(str(log_line))
+        # Split on newline
+        fragments = log_line.decode().split('\n')
 
+        # Print the leftover log line
+        log.debug(remnant + fragments.pop(0))
+        
+        # Save the last fragment of this element; it must not be newline terminated, and thus should not be printed yet
+        if len(fragments):
+            remnant = fragments.pop()
+        else:
+            remnant = ''
 
+        # Print the remaining log lines
+        for fragment in fragments:
+            log.debug(fragment)
 
 
 def generate_images(video_path, images_path, sample_rate, image_name_pattern):
     images_path.mkdir(parents=True, exist_ok=True)
+    # TODO: catch stdout in fastlog.debug
     return subprocess.call(['ffmpeg', '-i', video_path, '-vf', f'fps={sample_rate}', images_path / image_name_pattern])
 
 
-def generate_sparse(images_path, database_path, sparse_path):
+def generate_sfm_colmap(images_path, database_path, sparse_path):
     sparse_path.mkdir(parents=True, exist_ok=True)
     database_path.touch(exist_ok=True)
 
@@ -43,22 +69,46 @@ def generate_sparse(images_path, database_path, sparse_path):
     db_mount = docker.types.Mount('/data/database.db', str(database_path.absolute()), type='bind')
 
     # TODO: pull first, make logs visible    
-    
-    print('running feature extraction...')
-    feature_extraction_command = 'colmap feature_extractor --database_path /data/database.db --image_path /data/images'
-    run_in_docker(feature_extraction_command, 'colmap/colmap:latest', [images_mount, sparse_mount, db_mount])
+    with log.indent():
+        log.info('running feature extraction...')
+        with log.indent():
+            feature_extraction_command = 'colmap feature_extractor --database_path /data/database.db --image_path /data/images'
+            run_in_docker(feature_extraction_command, 'colmap/colmap:latest', [images_mount, sparse_mount, db_mount])
 
-    print('running matcher...')
-    matcher_command = 'colmap exhaustive_matcher --database_path /data/database.db'
-    run_in_docker(matcher_command, 'colmap/colmap:latest', [images_mount, sparse_mount, db_mount])
+        log.info('running matcher...')
+        with log.indent():
+            matcher_command = 'colmap exhaustive_matcher --database_path /data/database.db'
+            run_in_docker(matcher_command, 'colmap/colmap:latest', [images_mount, sparse_mount, db_mount])
 
-    print('running mapper...')
-    mapper_command = 'colmap mapper --database_path /data/database.db --image_path /data/images --output_path /data/sparse'
-    run_in_docker(mapper_command, 'colmap/colmap:latest', [images_mount, sparse_mount, db_mount])
+        log.info('running mapper...')
+        with log.indent():
+            mapper_command = 'colmap mapper --database_path /data/database.db --image_path /data/images --output_path /data/sparse'
+            run_in_docker(mapper_command, 'colmap/colmap:latest', [images_mount, sparse_mount, db_mount])
 
-    
 
-def generate_ply(images_path, sparse_path, ply_path, num_splats):
+def generate_sfm_odm(images_path, opensfm_path):
+    opensfm_path.mkdir(exist_ok=True)
+
+    images_mount = docker.types.Mount('/data/images', str(images_path.absolute()), type='bind')
+    odm_mount = docker.types.Mount('/data/opensfm', str(opensfm_path.absolute()), type='bind')
+
+    # TODO: pull first, make logs visible    
+    log.info('running odm...')
+    with log.indent():
+        odm_command = '--project-path / data --skip-orthophoto --skip-report --matcher-neighbors 7 --matcher-order 7'
+        run_in_docker(odm_command, 'opendronemap/odm', [images_mount, odm_mount])
+
+
+    # openMVG_main_SfMInit_ImageListing -d /home/qbowers/source_control/drone-data-processing/depend/openMVG/src/openMVG/exif/sensor_width_database/sensor_width_camera_database.txt -i images/ -o matches -x 2400
+    # openMVG_main_ComputeFeatures -i matches/sfm_data.json -o matches
+    # openMVG_main_ComputeMatches -i matches/sfm_data.json -o matches
+    # openMVG_main_SfM -i matches/sfm_data.json -m matches -o reconstruction -s INCREMENTAL
+    # open_splat_command = f'/code/build/opensplat /data/reconstruction/matches -n {num_splats} -o /data/output.splat'
+    # run_in_docker(open_splat_command, 'open_splat:latest', mounts=[images_mount, splat_mount, mvg_mount])
+
+
+
+def generate_ply(mounts, num_splats):
     client = docker.from_env()
 
     # Check that our custom image exists
@@ -70,46 +120,118 @@ def generate_ply(images_path, sparse_path, ply_path, num_splats):
     
     # Build the image
     if not image_exists:
-        print('building image...')
+        log.info('Building opensplat image. This may take a while...')
         # TODO: Logs
         client.images.build(path='depend/OpenSplat', tag='open_splat:latest')
     
     ply_path.touch(exist_ok=True)
 
     # Run the image
-    images_mount = docker.types.Mount('/data/images', str(images_path.absolute()), type='bind')
-    sparse_mount = docker.types.Mount('/data/sparse', str(sparse_path.absolute()), type='bind')
-    db_mount = docker.types.Mount('/data/output.splat', str(ply_path.absolute()), type='bind')
     
-    print('running opensplat')
-    open_splat_command = f'/code/build/opensplat /data -n {num_splats} -o /data/output.splat'
-    run_in_docker(open_splat_command, 'open_splat:latest', mounts=[images_mount, sparse_mount, db_mount])
+    log.info('running opensplat')
+    with log.indent():
+        open_splat_command = f'/code/build/opensplat /data -n {num_splats} -o /data/output.splat'
+        run_in_docker(open_splat_command, 'open_splat:latest', mounts=mounts)
 
 
 if __name__ == '__main__':
-    video_path = Path('gsplat_data/input/gopro/trimmed.mp4')
-    images_path = Path('gsplat_data/output/gopro/input')
-    sparse_path = Path('gsplat_data/output/gopro/sparse')
-    database_path = Path('gsplat_data/output/gopro/database.db')
-    ply_path = Path('gsplat_data/output/gopro/splat.ply')
+    sfm_options = {
+        'colmap': generate_sfm_colmap,
+        'odm': generate_sfm_odm
+    }
 
-    tic = time.time()
-    if not images_path.exists():
-        generate_images(video_path, images_path, 5, "out%d.png")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--sfm', choices=sfm_options.keys())
+
+    parser.add_argument('-v', action='count')
+    parser.add_argument('--verbosity', type=int, default=1)
+
+    args = parser.parse_args()
+
+    # Process verbosity args
+    verbosity = args.v if args.v else args.verbosity
+    verbosity = min(verbosity, len(log_level_options)-1)
+    log.setLevel(log_level_options[verbosity])
+
+
+    video_path = Path(f'gsplat_data/input/{args.dataset}.mp4')
+    output_path = Path(f'gsplat_data/output/{args.dataset}')
+
+    images_path =       output_path / 'images'
+
+    sparse_path =       output_path / 'sparse'
+    database_path =     output_path / 'database.db'
     
+    odm_path =          output_path / 'odm'
+    
+    openmvg_path =      output_path / 'reconstruction'
+    
+    ply_path =          output_path / 'splat.ply'
+
+
+    # FFMPEG
+    # TODO: this step will need to do EXIF data tagging as well. TBD how we will transmit that info. Likely it will be encoded as subtitles
+    # TODO: ODM may be able to handle EXIF tagging
+    tic = time.time()
+    if images_path.exists():
+        log.info(f'Skipping - Image Sampling: already exists at {images_path}')
+    else:
+        log.info(f'Running Image Sampling at {images_path}')
+        generate_images(video_path, images_path, 5, "out%d.png")
     generate_images_time = time.time() - tic
 
+    # SFM
     tic = time.time()
-    if not sparse_path.exists():
-        generate_sparse(images_path, database_path, sparse_path)
+    if args.sfm == 'colmap':
+        if sparse_path.exists():
+            log.info(f'Skipping - SFM: already exists at {sparse_path}')
+        else:
+            log.info(f'Running {args.sfm} at {sparse_path}')
+            generate_sfm_colmap(images_path, database_path, sparse_path)
+
+    elif args.sfm == 'odm':
+        if odm_path.exists():
+            log.info(f'Skipping - SFM: already exists at {odm_path}')
+        else:
+            log.info(f'Running {args.sfm} at {odm_path}')
+            generate_sfm_odm(images_path, odm_path)
+
+        if not sparse_path.exists():
+            generate_sfm_odm(images_path, odm_path)
     generate_sparse_time = time.time() - tic
 
+
+    # PLY
     tic = time.time()
-    if not ply_path.exists():
-        generate_ply(images_path, sparse_path, ply_path, 100_00)
+    if ply_path.exists():
+        log.info(f'Skipping - OpenSplat: already exists at {ply_path}')
+    else:
+        log.info(f'Running OpenSplat at {ply_path}')
+
+        mounts = [
+            docker.types.Mount('/data/images', str(images_path.absolute()), type='bind'),
+            docker.types.Mount('/data/output.splat', str(ply_path.absolute()), type='bind')
+        ]
+
+        if args.sfm == 'colmap':
+            mounts.append(docker.types.Mount('/data/sparse', str(sparse_path.absolute()), type='bind'))
+
+        elif args.sfm == 'odm':
+            mounts.append(docker.types.Mount('/data/opensfm', str(odm_path.absolute()), type='bind'))
+
+        # elif args.sfm == 'openMVG':
+        #    mounts.append(docker.types.Mount('/data/reconstruction', str(openmvg_path.absolute()), type='bind'))
+
+        generate_ply(mounts, 100_000)
     generate_ply_time = time.time() - tic
 
+
+    # REPORTING
     print(f'generate_images_time: {generate_images_time:.2f}')
     print(f'generate_sparse_time: {generate_sparse_time:.2f}')
     print(f'generate_ply_time:    {generate_ply_time:.2f}')
     print(f'total time elapsed:   {(generate_images_time + generate_sparse_time + generate_ply_time):.2f}')
+    print(f'ply is at:            {ply_path}')
